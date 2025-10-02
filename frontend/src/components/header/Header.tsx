e461a3f0df461d30cdf72fc7898ab5066a999811
denysmalaiko <malaiko.denis@gmail.com>
2025-10-02 20:19:41 +0200

    Backend | Frontend | Add 'Sign Out'

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,7 +1,34 @@
-import { Link } from 'react-router-dom';
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from '../../store';
+import { toast } from "react-toastify";
+import { signOutUser } from '../../store/auth/authThunks';
+
+import { ApiResponse } from "../../models/ApiResponse";
+
 
 function Header() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user, isAuthenticated } = useSelector((state: RootState) => state.authModule)
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+
+    try {
+      const response: any = await dispatch(
+        signOutUser()
+      ).unwrap();
+
+      navigate("/signIn");
+
+      toast.success(response.message);
+
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }
+
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between px-4 py-3">
@@ -13,8 +40,8 @@ function Header() {
         </nav>
 
         <nav>
-          <Link className="mr-3" to="/signIn">Sign In</Link>
-          <Link to="/signUp">Sign Up</Link>
+          {isAuthenticated && user ? <Link to="/profile/dashboard" className="text-blue-600">Welcome, { user.name }</Link> : <Link className="mr-1" to="/signIn">Sign In</Link>} /
+          {isAuthenticated && user ? <a className="ml-1 cursor-pointer" onClick={signOut}>Sign Out</a> : <Link to="/signUp" className="ml-1">Sign Up</Link>}
         </nav>
       </div>
     </header>
