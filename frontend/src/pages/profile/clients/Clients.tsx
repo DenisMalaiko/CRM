0bf3928849110dcb431d1768180e58b9c06d6514
denysmalaiko <malaiko.denis@gmail.com>
2025-10-21 18:53:46 +0200

    Frontend | Add ability to create client

--- frontend/src/pages/profile/clients/Clients.tsx
+++ frontend/src/pages/profile/clients/Clients.tsx
@@ -3,10 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../store";
 import CreateClientsDlg from "./createClientsDlg/CreateClientsDlg";
 import { getClients } from "../../../store/clients/clientsThunks";
+import { TClient } from "../../../models/Client";
 
 function Clients() {
   const dispatch = useDispatch<AppDispatch>();
   const [open, setOpen] = useState(false);
+  const [selectedClient, setSelectedClient] = useState<TClient | null>(null);
   const { clients } = useSelector((state: RootState) => state.clientsModule)
 
   useEffect(() => {
@@ -14,6 +16,7 @@ function Clients() {
   }, [dispatch]);
 
   const header = [
+    { name: "Image", key: "image" },
     { name: "First Name", key: "firstName" },
     { name: "Last Name", key: "lastName" },
     { name: "Email", key: "email" },
@@ -36,6 +39,12 @@ function Clients() {
           </button>
 
           <CreateClientsDlg
+            open={open}
+            onClose={() => {
+              setOpen(false);
+              setSelectedClient(null);
+            }}
+            client={selectedClient}
           ></CreateClientsDlg>
         </div>
       </section>
@@ -50,9 +59,36 @@ function Clients() {
                 ))}
               </tr>
             </thead>
-          </table>
 
-          <pre>{JSON.stringify(clients)}</pre>
+            <tbody className="divide-y divide-slate-100">
+            {clients && clients.map((item: TClient) => (
+              <tr key={item.id} className="hover:bg-slate-50">
+                <td className="px-4 py-3">
+                  <div className="h-10 w-10 bg-slate-200 rounded-lg" />
+                </td>
+                <td className="px-4 py-3 font-medium text-slate-900 text-left">{item.firstName}</td>
+                <td className="px-4 py-3 text-slate-600 text-left">{item.lastName}</td>
+                <td className="px-4 py-3 font-medium text-left">{item.email}</td>
+                <td className="px-4 py-3 text-left">{item.phoneNumber}</td>
+                <td className="px-4 py-3 text-slate-600 text-left">{item.address}</td>
+                <td className="px-4 py-3 text-slate-600 text-left">{item.role}</td>
+                <td className="px-4 py-3 text-left">
+                  <span className={`inline-flex items-start px-2 py-1 text-xs font-medium rounded-full`}>{item.role}</span>
+                </td>
+                <td className="px-4 py-3 text-right">
+                  <div className="flex items-center gap-2 justify-end">
+                    <button className="h-8 w-8 flex items-center justify-center rounded-lg border  text-slate-600 hover:bg-slate-50">
+                      ✎
+                    </button>
+                    <button className="h-8 w-8 flex items-center justify-center rounded-lg border text-rose-600 hover:bg-rose-50">
+                      🗑
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
