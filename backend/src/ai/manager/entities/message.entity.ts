687e969987687c2ed8caf6023a1a591c6ae4c8f1
denysmalaiko <malaiko.denis@gmail.com>
2025-12-01 19:56:32 +0100

    Backend | Add 'session' and 'message' table

---
+++ backend/src/ai/manager/entities/message.entity.ts
@@ -0,0 +1,11 @@
+import { MessageRolesUI } from "../../../enums/MessageRoles";
+import { Session } from "./session.entity";
+
+export class Message {
+  id: string;
+  sessionId: string;
+  session: Session;
+  role: MessageRolesUI;
+  message: string;
+  createdAt: Date;
+}
