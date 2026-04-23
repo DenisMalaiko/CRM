57dfbc0d841bb607073232e6e34f075de458f833
denysmalaiko <malaiko.denis@gmail.com>
2026-04-23 18:43:30 +0200

    feat: register photoAiModule in Redux store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -15,6 +15,7 @@ import galleryModule from "./gallery/gallerySlice";
 import ideaModule from "./idea/ideaSlice";
 import trendsModule from "./trends/trendsSlice";
 import ideaAiModule from "./ai/ideas/ideaAiSlice";
+import photoAiModule from "./ai/photo/photoAiSlice";
 import { api } from './api/api'
 
 export const store = configureStore({
@@ -34,6 +35,7 @@ export const store = configureStore({
     galleryModule: galleryModule,
     ideaModule: ideaModule,
     ideaAiModule: ideaAiModule,
+    photoAiModule: photoAiModule,
     trendsModule: trendsModule,
     [api.reducerPath]: api.reducer,
   },
