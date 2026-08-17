88d40ffef0eb7e5cbb2834d0840b49926a299b3c
denysmalaiko <malaiko.denis@gmail.com>
2026-08-17 20:02:10 +0200

    feat: add POST /facebook-report/fetch endpoint with auto-fetch via Apify

--- backend/src/modules/business/business.module.ts
+++ backend/src/modules/business/business.module.ts
@@ -3,9 +3,10 @@ import { BusinessController } from "./business.controller";
 import { BusinessService } from "./business.service";
 import { AuthModule } from "../auth/auth.module";
 import { InstagramModule } from "../instagram/instagram.module";
+import { FacebookModule } from "../facebook/facebook.module";
 
 @Module({
-  imports: [AuthModule, InstagramModule],
+  imports: [AuthModule, InstagramModule, FacebookModule],
   controllers: [BusinessController],
   providers: [BusinessService],
 })
